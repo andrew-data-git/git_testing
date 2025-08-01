111efe14e0bc860e464bd2088a5b3ad1d4fdd834
andrew <ajrowley3000@gmail.com>
2025-08-01 11:14:16 +0100

    populated test

--- test_script.py
+++ test_script.py
@@ -1,2 +1,14 @@
-import script
+from script import threshold_df
+import pandas as pd
 
+
+def test_threshold_df():
+    # test with synth data below threshold, threshold to 5 rows
+    df_1 = pd.DataFrame([[0,1],[0,1]], columns=['col1','col2'])
+    df_2 = pd.DataFrame([[0,1],[0,1],[None,None],[None,None],[None,None]], columns=['col1','col2'])
+    assert df_2.equals(threshold_df(df_1, threshold=5))
+
+    # test with synth data above threshold, threshold to 2 rows
+    df_3 = pd.DataFrame([[0,1],[0,1],[0,1],[0,1],[0,1]], columns=['col1','col2'])
+    df_4 = pd.DataFrame([[0,1],[0,1]], columns=['col1','col2'])
+    assert df_4.equals(threshold_df(df_3, threshold=2))
